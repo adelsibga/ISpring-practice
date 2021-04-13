1b4871808104da4c2bf0e79f4f552154ccb2d4ca
adelsibga <79917869+adelsibga@users.noreply.github.com>
2021-04-13 17:34:27 +0300

    Bug fix

--- LW1/about_me/src/Controller/PageController.php
+++ LW1/about_me/src/Controller/PageController.php
@@ -12,14 +12,14 @@ class PageController extends AbstractController
 {
   public function page(): Response
   {
-	  $images = new ImageProvider();
-	  $firstTheme = $images->getPhotosUrls('АСБ_мужской_баскетбол');
-	  $secondPhotos = $images->getPhotosUrls('Математика');
-	  $thirdPhotos = $images->getPhotosUrls('Готовка');
-		  return $this->render('/about_me/index.html.twig', [
-		  'firstPhotos' => $firstTheme,
-		  'secondPhotos' => $secondPhotos,
-		  'thirdPhotos' => $thirdPhotos,
-	  ]);
+      $images = new ImageProvider();
+      $firstTheme = $images->getPhotosUrls('АСБ_мужской_баскетбол');
+      $secondPhotos = $images->getPhotosUrls('Математика');
+      $thirdPhotos = $images->getPhotosUrls('Готовка');
+          return $this->render('/about_me/index.html.twig', [
+          'firstPhotos' => $firstTheme,
+          'secondPhotos' => $secondPhotos,
+          'thirdPhotos' => $thirdPhotos,
+      ]);
   }
 }
