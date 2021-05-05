eb1a04ec6737b61dd24959c594d0a3e5341e5b29
adelsibga <79917869+adelsibga@users.noreply.github.com>
2021-05-05 17:27:10 +0300

    Indentation fix

--- LW1/about_me/src/Controller/PageController.php
+++ LW1/about_me/src/Controller/PageController.php
@@ -1,8 +1,6 @@
 <?php
 
-
 namespace App\Controller;
-
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,16 +8,16 @@ use App\Module\ImageProvider;
 
 class PageController extends AbstractController
 {
-  public function page(): Response
-  {
-      $images = new ImageProvider();
-      $firstTheme = $images->getPhotosUrls('АСБ_мужской_баскетбол');
-      $secondPhotos = $images->getPhotosUrls('Математика');
-      $thirdPhotos = $images->getPhotosUrls('Готовка');
-          return $this->render('/about_me/index.html.twig', [
-          'firstPhotos' => $firstTheme,
-          'secondPhotos' => $secondPhotos,
-          'thirdPhotos' => $thirdPhotos,
-      ]);
-  }
+    public function page(): Response
+    {
+        $images = new ImageProvider();
+        $firstPhotos = $images->getPhotosUrls('АСБ_мужской_баскетбол');
+        $secondPhotos = $images->getPhotosUrls('Математика');
+        $thirdPhotos = $images->getPhotosUrls('Готовка');
+        return $this->render('/about_me/index.html.twig', [
+            'firstPhotos' => $firstPhotos,
+            'secondPhotos' => $secondPhotos,
+            'thirdPhotos' => $thirdPhotos,
+        ]);
+    }
 }
--- LW1/about_me/src/Module/ImageProvider.php
+++ LW1/about_me/src/Module/ImageProvider.php
@@ -1,28 +1,29 @@
 <?php
+
 namespace App\Module;
+
 use IvanUskov\ImageSpider\ImageSpider;
 
 class ImageProvider implements ImageProviderInterface
 {
-	private array $urls;
+    private const QUANTITY_PHOTOS = 5;
+    private array $urls;
 
-	public function getUrls(string $theme): void        // получаем случайные фото по запросу
-	{
-		$this->urls = ImageSpider::find($theme);
-		return;
-	}
+    public function getUrls(string $theme): void        // получаем случайные фото по запросу
+    {
+        $this->urls = ImageSpider::find($theme);
+    }
 
-	public function getPhotosUrls(string $theme): array // получаем 5 случайных фото по теме 
-	{
-		$this->getUrls($theme);
-		for ($i = 0; $i < 5; ++$i)
-		{ 
-			$length = count($this->urls);
-			$index = rand(0, ($length - 1));
-			$result[] = $this->urls[$index];
-			unset($this->urls[$index]);                 // убираем из массива ссылку, чтобы не повторялась      
-		}
+    public function getPhotosUrls(string $theme): array // получаем 5 случайных фото по теме
+    {
+        $this->getUrls($theme);
+        for ($i = 0; $i < self::QUANTITY_PHOTOS; ++$i) {
+            $length = count($this->urls);
+            $index = rand(0, ($length - 1));
+            $result[] = $this->urls[$index];
+            unset($this->urls[$index]);                 // убираем из массива ссылку, чтобы не повторялась
+        }
 
-		return $result; 
-	}
+        return $result;
+    }
 }
