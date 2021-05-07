95af5f8d25aacfb1f6ce81b8ad67929317c41d8a
adelsibga <79917869+adelsibga@users.noreply.github.com>
2021-05-07 14:11:47 +0300

    Fix mistakes

--- LW1/about_me/src/Module/ImageProvider.php
+++ LW1/about_me/src/Module/ImageProvider.php
@@ -7,23 +7,16 @@ use IvanUskov\ImageSpider\ImageSpider;
 class ImageProvider implements ImageProviderInterface
 {
     private const QUANTITY_PHOTOS = 5;
-    private array $urls;
 
-    public function getUrls(string $theme): void        // получаем случайные фото по запросу
+    public function getPhotosUrls(string $theme): array
     {
-        $this->urls = ImageSpider::find($theme);
+        $urls = ImageSpider::find(urlencode($theme));
+        return $this->randomizeAndLimit($urls);
     }
 
-    public function getPhotosUrls(string $theme): array // получаем 5 случайных фото по теме
+    private function randomizeAndLimit(array $urls): array
     {
-        $this->getUrls($theme);
-        for ($i = 0; $i < self::QUANTITY_PHOTOS; ++$i) {
-            $length = count($this->urls);
-            $index = rand(0, ($length - 1));
-            $result[] = $this->urls[$index];
-            unset($this->urls[$index]);                 // убираем из массива ссылку, чтобы не повторялась
-        }
-
-        return $result;
+        shuffle($urls);
+        return array_slice($urls, 0, self::QUANTITY_PHOTOS);
     }
 }
