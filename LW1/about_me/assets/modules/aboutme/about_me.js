42fa1efee1fe3dc006a8b82df8d6e0a0c325b13c
adelsibga <79917869+adelsibga@users.noreply.github.com>
2021-05-21 17:29:05 +0300

    slider without dots and work arrows

--- LW1/about_me/assets/modules/aboutme/about_me.js
+++ LW1/about_me/assets/modules/aboutme/about_me.js
@@ -1,13 +1,22 @@
-import './about_me.css';
+require('./about_me.css');
+require('./components/slider/slider.js');
 
 import $ from 'jquery';
 
-import Slider from '/components/slider';
+import Slider from "./components/slider/slider";
 
-$(document).ready(() => {
-    // Iterate through all the .slider blocks and make them dynamic
-    $('.slider').each((i, obj) => {
-        const slider = new Slider($(obj));
-        slider.initialize();
-    })
-});
+const slider1 = new Slider(".slidewrapper_1", ".images_1",
+    ".next-btn_1", ".prev-btn_1", ".slide-nav-btn_1"
+);
+const slider2 = new Slider(".slidewrapper_2", ".images_2",
+    ".next-btn_2", ".prev-btn_2", ".slide-nav-btn_2"
+);
+const slider3 = new Slider(".slidewrapper_3", ".images_3",
+    ".next-btn_3", ".prev-btn_3", ".slide-nav-btn_3"
+);
+
+$(document).ready(function () {
+    slider1.initialize();
+    slider2.initialize();
+    slider3.initialize();
+})
